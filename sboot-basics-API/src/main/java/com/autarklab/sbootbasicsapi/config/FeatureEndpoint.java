8696713e0d8786c10d28b399769442c27ae19139
yoelelectron <manelauft@gmail.com>
2023-09-20 16:30:25 -0500

    basics actuators

---
+++ sboot-basics-API/src/main/java/com/autarklab/sbootbasicsapi/config/FeatureEndpoint.java
@@ -0,0 +1,46 @@
+package com.autarklab.sbootbasicsapi.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.actuate.endpoint.annotation.Endpoint;
+import org.springframework.boot.actuate.endpoint.annotation.ReadOperation;
+import org.springframework.boot.actuate.endpoint.annotation.Selector;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@Endpoint(id = "features")
+public class FeatureEndpoint {
+
+    private final Map<String, Feature> featureMap =
+            new ConcurrentHashMap<>();
+
+    public FeatureEndpoint() {
+        featureMap.put("Department", new Feature(true));
+        featureMap.put("User", new Feature(true));
+        featureMap.put("Guest", new Feature(true));
+        featureMap.put("Authentication", new Feature(false));
+        featureMap.put("authorization", new Feature(false));
+
+    }
+
+    @ReadOperation
+    public Map<String, Feature> features() {
+        return featureMap;
+    }
+
+    @ReadOperation
+    public Feature feature(@Selector String featureName) {
+        return featureMap.get(featureName);
+    }
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Feature {
+        private boolean isEnabled;
+    }
+}
